fb71145feadb38e83fcf83b64f08c5b4988ac4b1
Jose Vitor <98997905+vitorsantabi@users.noreply.github.com>
2025-09-10 12:55:16 -0300

    Add files via upload

--- components/icon.tsx
+++ components/icon.tsx
@@ -1,8 +1,9 @@
 import { BlurView } from "expo-blur";
 import React from "react";
 import { Image, StyleSheet, Text, View, useWindowDimensions } from "react-native";
-
 const icon = require("../assets/photos/icon.jpg");
+import { useFonts as customFonts } from '../utils/fonts';
+import { useFonts } from 'expo-font';
 
 export default function Icon(): React.JSX.Element {
   const { width } = useWindowDimensions();
@@ -12,13 +13,23 @@ export default function Icon(): React.JSX.Element {
   const isMediumScreen = width >= 400 && width < 768;
 
   const iconSize = isSmallScreen ? 80 : isMediumScreen ? 90 : 100;
-  const titleFontSize = isSmallScreen ? 28 : isMediumScreen ? 32 : 35;
+  const titleFontSize = isSmallScreen ? 35 : isMediumScreen ? 40 : 45;
   const subtitleFontSize = isSmallScreen ? 13 : isMediumScreen ? 14 : 15;
   const headerWidth = isSmallScreen ? width * 0.9 : "85%";
   const headerHeight = isSmallScreen ? 110 : isMediumScreen ? 120 : 130;
 
+  // Carrega todas as fontes (Poppins e Caprasimo)
+  const [fontsLoaded] = useFonts({
+    ...customFonts.Poppins,
+    ...customFonts.Caprasimo
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <BlurView intensity={50} tint="extraLight" style={[styles.container, { width: headerWidth, height: headerHeight }]}>
+    <BlurView intensity={20} tint="extraLight" style={[styles.container, { width: headerWidth, height: headerHeight }]}>
       <Image
         source={icon}
         style={[
@@ -32,34 +43,12 @@ export default function Icon(): React.JSX.Element {
       />
       <View style={styles.textContainer}>
         <Text
-          style={{
-            color: "#fff",
-            fontSize: titleFontSize,
-            fontWeight: "bold",
-            marginTop: 20,
-            marginLeft: 20,
-            textShadowColor: "rgba(179, 22, 158, 0)",
-            textShadowOffset: { width: 1, height: 1 },
-            textShadowRadius: 0,
-          }}
+          style={[styles.Titulo, { fontSize: titleFontSize }]}
         >
           Electra Heart
         </Text>
         <Text
-          style={{
-            color: "#fff",
-            fontSize: subtitleFontSize,
-            fontWeight: "bold",
-            marginTop: 20,
-            marginLeft: 20,
-            opacity: 0.7,
-            position: "absolute",
-            bottom: 2,
-            textShadowRadius: 10,
-            fontStyle: "italic",
-            letterSpacing: 1,
-            lineHeight: 20,
-          }}
+          style={[styles.subtitle, { fontSize: subtitleFontSize }]}
         >
           Um Manifesto Pop
         </Text>
@@ -78,6 +67,8 @@ const styles = StyleSheet.create({
     padding: 10,
     zIndex: 1000,
     flexDirection: "row",
+    borderColor: "rgba(85, 10, 62, 0.07)",
+    borderWidth: 3,
   },
   icon: {
     borderRadius: 50,
@@ -92,4 +83,27 @@ const styles = StyleSheet.create({
     marginBottom: 35,
     marginLeft: -15,
   },
+  Titulo: {
+    color: "#f0b6ffff",
+    fontFamily: "Caprasimo_400Regular", // Fonte Caprasimo
+    marginTop: 20,
+    marginLeft: 20,
+    textShadowColor: "rgba(0, 0, 0, 1)",
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 10,
+  },
+  subtitle: {
+    color: "#fff",
+    fontFamily: "Poppins_600SemiBold", // Fonte Poppins
+    fontWeight: "bold",
+    marginTop: 20,
+    marginLeft: 20,
+    opacity: 0.7,
+    position: "absolute",
+    bottom: 2,
+    textShadowRadius: 10,
+    fontStyle: "italic",
+    letterSpacing: 1,
+    lineHeight: 20,
+  }
 });
