2cbb694bd823999bd9de76f1db7742d32b740ea0
Jose Vitor <98997905+vitorsantabi@users.noreply.github.com>
2025-09-10 12:55:33 -0300

    Add files via upload

---
+++ utils/fonts.ts
@@ -0,0 +1,32 @@
+import {
+    Poppins_100Thin,
+    Poppins_200ExtraLight,
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+    Poppins_900Black,
+} from "@expo-google-fonts/poppins";
+
+import {
+    Caprasimo_400Regular,
+} from "@expo-google-fonts/caprasimo";
+
+export const useFonts = {
+    Poppins: {
+        Poppins_100Thin,
+        Poppins_200ExtraLight,
+        Poppins_300Light,
+        Poppins_400Regular,
+        Poppins_500Medium,
+        Poppins_600SemiBold,
+        Poppins_700Bold,
+        Poppins_800ExtraBold,
+        Poppins_900Black
+    },
+    Caprasimo: {
+        Caprasimo_400Regular,
+    }
+};
